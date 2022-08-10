9d922fd7fe12366266b2b0d59d425ebb297bb5d5
gene5487 <gene5487@gmail.com>
2022-08-10 18:36:36 +0800

    new feature: generate skill icon file

--- AutoNodestone.py
+++ AutoNodestone.py
@@ -12,6 +12,7 @@ import cv2
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+import numpy as np
 
 
 # parameters for locate_25node_at_once
@@ -25,6 +26,10 @@ trinode_index_connvert_table = [[0,   1,  2,  3],
                                 [14, 15, 16, 17],
                                 [18, 19, 20],
                                 [21, 22, 23, 24]]
+
+# parameters for get_skill_icon
+offset = [-39, -16]  # from left top of skill_separation_line, to left top of skill icon
+threshold = 0.95
 
 
 def sound_effect(sound_type):
@@ -72,8 +77,11 @@ def locate_25node_at_once(skill_icon_path_list, page, n_trinode_inlist):
 
     for trinode in trinode_name_list:
         if len(trinode) == 2:
-            print("Warning! Only two of skill is detected for a trinode, "
+            print("Warning! Only 2 of skill is detected for a trinode, "
                   "please make sure all trinode is unlocked before scanning.")
+        if len(trinode) > 3:
+            print(rf"Warning! More than 3 skill is detected for a trinode, "
+                  rf"please make sure all duplicate/unneeded skill icon at {my_job_path} are deleted manually before scanning.")
     scanned_ok_indices = [i for i in range(len(trinode_name_list)) if len(trinode_name_list[i]) == 3]
     trinode_name_list = [trinode_name_list[i] for i in scanned_ok_indices]
     trinode_position = [trinode_position[i] for i in scanned_ok_indices]
@@ -95,6 +103,54 @@ def locate_25node_at_once(skill_icon_path_list, page, n_trinode_inlist):
     return trinode_name_list, trinode_skill, trinode_first
 
 
+def get_skill_icon(my_job, skill_icon_page, scan_region):
+    template = cv2.imread(r'./template_icon/left_square_bracket.png')
+    h, w = template.shape[:-1]
+    pyautogui.screenshot(f'SkillIconMatchingResult_{my_job}_{skill_icon_page}.png', region=scan_region)
+    img = cv2.imread(f'SkillIconMatchingResult_{my_job}_{skill_icon_page}.png')
+    # img = cv2.imdecode(np.fromfile(f'SkillIconMatchingResult_{my_job}_{skill_icon_page}.png', dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+    res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(res >= threshold)  # return indices that meet condition
+    for i, pt in enumerate(zip(*loc[::-1])):  # Switch columns and rows
+        left, top = pt[0] + offset[0], pt[1] + offset[1]
+        cv2.imwrite(rf'./skill_icon/{my_job}/SkillIcon_{skill_icon_page}_{i}.png',
+                    img[top: (top + 32), left: (left + 32)], (cv2.IMWRITE_PNG_COMPRESSION, 0))
+
+    for i, pt in enumerate(zip(*loc[::-1])):  # Switch columns and rows
+        left, top = pt[0] + offset[0], pt[1] + offset[1]
+        # red box for skill_separation_line
+        cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+        # green box for skill icon
+        cv2.rectangle(img, (left, top), (left + 32, top + 32), (0, 255, 0), 2)
+
+    cv2.imwrite(f'SkillIconMatchingResult_{my_job}_{skill_icon_page}.png', img)
+    return len(loc[0])  # return how many skill icon is detected
+
+
+def update_skill_icon(my_job_path):
+    # update skill_img_list after get_skill_icon
+    skill_img_list = [rf"{my_job_path}/{img_name}" for img_name in listdir(my_job_path) if
+                      isfile(rf"{my_job_path}/{img_name}")]
+    n_skill = len(skill_img_list)
+
+    # variable for locate_25node_at_once
+    # cv2.imread(img) not support Unicode characters (chinese, 墨玄),
+    # use cv2.imdecode(np.fromfile(img, dtype=np.uint8), cv2.IMREAD_UNCHANGED) instead
+    bl_skill_img_list = [cv2.imread(img)
+                         [bl_cor[1]: bl_cor[1] + tm_cor[3], bl_cor[0]:bl_cor[0] + bl_cor[2]]
+                         for img in skill_img_list]
+    tm_skill_img_list = [cv2.imread(img)
+                         [tm_cor[1]: tm_cor[1] + tm_cor[3], tm_cor[0]:tm_cor[0] + tm_cor[2]]
+                         for img in skill_img_list]
+    br_skill_img_list = [cv2.imread(img)
+                         [br_cor[1]: br_cor[1] + br_cor[3], br_cor[0]:br_cor[0] + br_cor[2]]
+                         for img in skill_img_list]
+    skill_name_list = [rf'{img.split("/")[-1].split(".")[0]}'
+                       for img in skill_img_list]
+    all_partial_skill_img_list = [bl_skill_img_list, tm_skill_img_list, br_skill_img_list]
+    return skill_img_list, n_skill, bl_skill_img_list, tm_skill_img_list, br_skill_img_list, skill_name_list, all_partial_skill_img_list
+
+
 print("==========Stage 1 : Select your job==========")
 root = Tk()
 root.withdraw()
@@ -103,32 +159,25 @@ my_job_path = askdirectory(title='Select your job', initialdir=r'./skill_icon', 
 my_job = my_job_path.split("/")[-1]
 print(f'You are {my_job}')
 
-skill_img_list = [rf"{my_job_path}/{img_name}" for img_name in listdir(my_job_path) if
-                  isfile(rf"{my_job_path}/{img_name}")]
-n_skill = len(skill_img_list)
+skill_img_list, n_skill, bl_skill_img_list, tm_skill_img_list, br_skill_img_list, \
+    skill_name_list, all_partial_skill_img_list = update_skill_icon(my_job_path)
+print(f'{n_skill} skill icon file in {my_job_path}')
+
 trinode_skill = []
 trinode_first = []
 
-# variable for locate_25node_at_once
-bl_skill_img_list = [cv2.imread(img)[bl_cor[1]: bl_cor[1] + tm_cor[3], bl_cor[0]:bl_cor[0] + bl_cor[2]]
-                     for img in skill_img_list]
-tm_skill_img_list = [cv2.imread(img)[tm_cor[1]: tm_cor[1] + tm_cor[3], tm_cor[0]:tm_cor[0] + tm_cor[2]]
-                     for img in skill_img_list]
-br_skill_img_list = [cv2.imread(img)[br_cor[1]: br_cor[1] + br_cor[3], br_cor[0]:br_cor[0] + br_cor[2]]
-                     for img in skill_img_list]
-skill_name_list = [rf'{img.split("/")[-1].split(".")[0]}'
-                   for img in skill_img_list]
-all_partial_skill_img_list = [bl_skill_img_list, tm_skill_img_list, br_skill_img_list]
 part_list = ['bl', 'tm', 'br']
 page = 0
+skill_icon_page = 0
 
 print("==========Stage 2 : Input your Nodestone==========")
 print("Let program scan through all Nodestone you have\n")
+print("Press 'g' to generate skill icon png file (Put cursor on the Nodestone to show the 3 skill icon)")
 print("Press 'i' to scan the Nodestone currently on screen (Put cursor on the Nodestone to show the 3 skill icon)")
 print("Press 'b' to scan whole V-matrix page (Talk to V core master, open the 25-core-per-page UI)")
 print("Press 's' to save scanned Nodestone to file")
 print("Press 'l' to load scanned Nodestone from file")
-print("Press 'e' to end this stage")
+print("Press 'e' to end this stage\n")
 K = keyboard.read_key()
 while K != 'e':
     if K == 's':
@@ -154,6 +203,7 @@ while K != 'e':
             cursor_position = pyautogui.position()
             scan_region = [cursor_position[0] - 10, cursor_position[1], 120, 240]
             pyautogui.screenshot('scan_region.png', region=scan_region)
+            # pyautogui.locateCenterOnScreen doesn't support chinese file name!
             position = pyautogui.locateCenterOnScreen(skill_icon, region=scan_region, confidence=0.8)
             if position is not None:
                 skill_indices.append(i)
@@ -177,8 +227,6 @@ while K != 'e':
             new_trinode_first[top_skill_index] = 1
             trinode_first.append(new_trinode_first)
             trinode_skill.append(new_trinode_skill)
-            # print(trinode_skill)
-            # print(trinode_first)
 
         else:
             print("Unable to recognize Nodestone skill !")
@@ -197,7 +245,20 @@ while K != 'e':
             sound_effect(1)
         else:
             sound_effect(0)
-        print(rf'{len(new_trinode_skill)} Nodestone are detected')
+        print(rf'{len(new_trinode_skill)} Nodestone are detected.')
+
+    elif K == 'g':
+        cursor_position = pyautogui.position()
+        scan_region = [cursor_position[0] - 10, cursor_position[1], 120, 240]
+        n_skill_icon = get_skill_icon(my_job=my_job, skill_icon_page=skill_icon_page, scan_region=scan_region)
+        if n_skill_icon != 0:
+            update_skill_icon(my_job_path)
+            skill_icon_page += 1
+            sound_effect(1)
+        else:
+            sound_effect(0)
+        print(rf'{n_skill_icon} skill icon are detected, saved at {my_job_path}.')
+        print(rf'Please delete duplicate/unneeded skill icon manually, and then restart the program.')
 
     K = keyboard.read_key()
 
@@ -252,7 +313,7 @@ while True:
     else:
         print("None of Nodestone combination can meet your requirement !")
 
-    print("\nPress 'r' to retry Stage3~4")
+    print("\nPress 'r' to retry Stage 3~4")
     print("Press 'q' to end this program")
 
     while True:
